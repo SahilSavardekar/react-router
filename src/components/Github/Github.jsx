53318c1a332e2023b7ad845c543a959bf0cbd845
Sahil Savardekar <sahilsavardekar2003@gmail.com>
2024-07-17 11:06:50 +0530

    react-router-dom

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+
+function Github() {
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/SahilSavardekar')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setData(data);
+        })
+    }, [])
+
+    
+  return (
+      <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github
+
+      </div>
+      
+  )
+}
+
+export default Github
